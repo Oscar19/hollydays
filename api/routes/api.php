947c2622026c26c64bfc140949c0329f969b7007
Oscar <oscanguera@gmail.com>
2024-10-18 12:12:12 +0200

    migraciones y rutas

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\entregasController;
+use App\Http\Controllers\vacacionesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/vacaciones{id}/realitzar', [vacacionesController::class, 'realitzar']);
+Route::delete('/vacaciones{id}/anular', [vacacionesController::class, 'anular']);
+Route::post('/entregas{id}/entregar', [entregasController::class, 'entregar']);
+Route::post('/entregas{id}/reentregar', [entregasController::class, 'reentregar']);
