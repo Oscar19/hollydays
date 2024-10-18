0ba07471a1d3ff57fcaff481648a6e66cdcd685c
Oscar <oscanguera@gmail.com>
2024-10-18 11:41:04 +0200

    creación de modelo vacaciones y tabla

---
+++ api/database/migrations/2024_10_18_093844_create_vacaciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pla_vacacionals', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('lloc');
+            $table->date('data');
+            $table->enum('tipus', ['Restaurant', 'Esport', 'Cultural', 'Visita', 'Altres']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacaciones');
+    }
+};
